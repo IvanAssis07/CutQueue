7cfb715f3dca9fb6d3492bfabd68bc7791cf3024
francisco neves <francisconevest@outlook.com>
2024-04-24 22:19:10 -0300

    Adding more tests

--- src/domains/openingHours/service/openingHoursService.test.ts
+++ src/domains/openingHours/service/openingHoursService.test.ts
@@ -126,6 +126,204 @@ describe('Create OpeningHoursService method', function() {
 
     });
 
+    test('Should throw a ConflictError for existing opening hour on the same day', async () => {
+      const data = {
+        id: 'opening_hours_id',
+        number: 0,
+        openingTime: '12:00',
+        closingTime: '23:30',
+        barbershopId: 'test_barbershop_id',
+        name: 'Opening hours test',
+        day: 0
+      };
+      const loggedUserId = 'test_owner_id';
+
+      const barbershop = {
+        id: 'test_barbershop_id',
+        ownerId: loggedUserId,
+        name: 'Test barbershop',
+        description: 'Test Description',
+        phone: '99999-9999',
+        address: 'Test address',
+      };
+
+      const existingOpeningHour = {
+        id: 'existingOpeningHour',
+        barbershopId: data.barbershopId,
+        day: data.day,
+        openingTime: '10:00',
+        closingTime: '18:00',
+      };
+
+      // Mock prisma to return existing opening hour
+      prismaMock.barbershop.findUnique.mockResolvedValue(barbershop);
+      prismaMock.openingHours.findFirst.mockResolvedValue(existingOpeningHour);
+
+      await expect(openingHoursService.create(data, loggedUserId)).rejects.toThrow(ConflictError);
+    });
+
+    test('Should return an empty array for a barbershop with no opening hours', async () => {
+      const barbershopId = 'test_barbershop_id';
+
+      // Mock prisma to return an empty array
+      prismaMock.openingHours.findMany.mockResolvedValue([]);
+
+      const openingHours = await openingHoursService.getOpeningHoursFromBarbershop(barbershopId);
+
+      expect(openingHours).toEqual([]);
+    });
+
+    test('Should throw an InvalidParamError if non-existing id', async () => {
+      const data = { id: 'opening_hours_id',
+                    day: 0,
+                    openingTime: '10:00',
+                    closingTime: '18:00',
+                    barbershopId: 'test_barbershop_id'};
+      const  openingHoursId = '';
+
+      const loggedUserId = '';
+
+
+      await expect(openingHoursService.updateOpeningHours(data,openingHoursId, loggedUserId)).rejects.toThrow(InvalidParamError);
+    });
+
+    test('Should throw an InvalidParamError if invalid date format', async () => {
+
+        const existingOpeningHour = {
+        id: 'opening_hours_id',
+        barbershopId: 'test_barbershop_id',
+        day: 0,
+        openingTime: '10:00',
+        closingTime: '18:00',
+        };
+
+        prismaMock.openingHours.findFirst.mockResolvedValue(existingOpeningHour);
+
+
+        const data = { id: existingOpeningHour.id,
+                    day: existingOpeningHour.day,
+                    openingTime: '10:05',
+                    closingTime: '18:00',
+                    barbershopId: existingOpeningHour.barbershopId};
+        const  openingHoursId = 'opening_hours_id';
+
+        const loggedUserId = '';
+
+
+        await expect(openingHoursService.updateOpeningHours(data,openingHoursId, loggedUserId)).rejects.toThrow(InvalidParamError);
+    });
+
+     test('Should throw an PermissionError if day dont have a barbershop', async () => {
+        const existingOpeningHour = {
+        id: 'opening_hours_id',
+        barbershopId: 'test_barbershop_id',
+        day: 0,
+        openingTime: '10:00',
+        closingTime: '18:00',
+        };
+
+        await prismaMock.openingHours.findFirst.mockResolvedValue(existingOpeningHour);
+
+
+        const data = { id: existingOpeningHour.id,
+                    day: existingOpeningHour.day,
+                    openingTime: '10:00',
+                    closingTime: '18:00',
+                    barbershopId: existingOpeningHour.barbershopId};
+        const  openingHoursId = 'opening_hours_id';
+
+        const loggedUserId = 'test_other_id';
+
+
+        await expect(openingHoursService.updateOpeningHours(data,openingHoursId, loggedUserId)).rejects.toThrow(InvalidParamError);
+    });
+
+
+    test('Should throw an PermissionError if loggedUserId is not the owner', async () => {
+        const barbershop = {
+            id: 'test_barbershop_id',
+            ownerId: 'test_owner_id',
+            name: 'Test barbershop',
+            description: 'Test Description',
+            phone: '99999-9999',
+            address: 'Test address',
+        };
+
+        await prismaMock.barbershop.findUnique.mockResolvedValue(barbershop);
+
+
+        const existingOpeningHour = {
+        id: 'opening_hours_id',
+        barbershopId: barbershop.id,
+        day: 0,
+        openingTime: '10:00',
+        closingTime: '18:00',
+        };
+
+        await prismaMock.openingHours.findFirst.mockResolvedValue(existingOpeningHour);
+
+
+        const data = { id: existingOpeningHour.id,
+                    day: existingOpeningHour.day,
+                    openingTime: '10:00',
+                    closingTime: '18:00',
+                    barbershopId: existingOpeningHour.barbershopId};
+        const  openingHoursId = 'opening_hours_id';
+
+        const loggedUserId = 'test_other_id';
+
+
+        await expect(openingHoursService.updateOpeningHours(data,openingHoursId, loggedUserId)).rejects.toThrow(PermissionError);
+    });
+
+    test('Should update the Opening Hour', async () => {
+        const barbershop = {
+            id: 'test_barbershop_id',
+            ownerId: 'test_owner_id',
+            name: 'Test barbershop',
+            description: 'Test Description',
+            phone: '99999-9999',
+            address: 'Test address',
+        };
+
+        await prismaMock.barbershop.findUnique.mockResolvedValue(barbershop);
+
+
+        const existingOpeningHour = {
+        id: 'opening_hours_id',
+        barbershopId: barbershop.id,
+        day: 0,
+        openingTime: '10:00',
+        closingTime: '18:00',
+        };
+
+        await prismaMock.openingHours.findFirst.mockResolvedValue(existingOpeningHour);
+
+
+        const data = { id: 'opening_other_hours_id',
+                    day: existingOpeningHour.day,
+                    openingTime: '10:00',
+                    closingTime: '18:00',
+                    barbershopId: existingOpeningHour.barbershopId};
+        const  openingHoursId = 'opening_hours_id';
+
+        const loggedUserId = barbershop.ownerId;
+
+        await expect(openingHoursService.updateOpeningHours(data,openingHoursId, loggedUserId)).resolves.not.toThrow();
+
+        expect(prismaMock.openingHours.update).toHaveBeenCalledWith({
+            where: {
+                id: openingHoursId
+            },
+            data: {
+                openingTime: data.openingTime,
+                closingTime: data.closingTime
+            }
+        });
+    });
+
+
+
 
 
 });
